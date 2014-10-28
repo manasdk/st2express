e9aa659f366371f8b3a95a2b1966be2f520c1e1f
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-10-28 14:24:05 -0700

    Add: Install sensu by default with st2express.

--- Vagrantfile
+++ Vagrantfile
@@ -1,31 +1,33 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
- 
+
 st2ver="0.5.1"
- 
+
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
- 
+
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
- 
+
     config.vm.box = "ubuntu/trusty64"
     config.vm.hostname = "st2express"
- 
+
     config.vm.define "st2express" do |q|
     end
- 
+
     config.vm.provider :virtualbox do |vb|
       vb.name = "st2express"
       vb.memory = 2048
       vb.cpus = 1
     end
- 
+
     # Configure a private network
     config.vm.network "private_network", ip: "172.168.90.50"
- 
+
     # Start shell provisioning
     config.vm.provision :shell, :path => "mistral.sh"
     config.vm.provision :shell, :inline => "curl -sS -k -O https://ops.stackstorm.net/releases/st2/scripts/st2_deploy.sh"
     config.vm.provision :shell, :inline => "bash st2_deploy.sh #{st2ver}"
     config.vm.provision :shell, :path => "rsyslog.sh"
+    config.vm.provision :shell, :path => "sensu_server.sh"
+    config.vm.provision :shell, :path => "sensu_client.sh"
 end
---
+++ sensu_client.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+HOSTNAME=`hostname`
+IPADDR=`ifconfig | grep -A1 'eth1' | grep -A1 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1`
+
+prereqs(){
+
+  wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | sudo apt-key add -
+
+  if [ ! -f /etc/apt/sources.list.d/sensu.list ]
+  then
+    echo "deb http://repos.sensuapp.org/apt sensu main" > /etc/apt/sources.list.d/sensu.list
+  fi
+  apt-get update
+}
+
+install_sensu() {
+  apt-get -y install sensu
+}
+
+configure_sensu(){
+  mkdir -p /etc/sensu/ssl
+  cp -f /vagrant/sensu/ssl_certs/client/cert.pem /etc/sensu/ssl/
+  cp -f /vagrant/sensu/ssl_certs/client/key.pem /etc/sensu/ssl/
+
+  cp /vagrant/sensu/rabbitmq.json /etc/sensu/conf.d/rabbitmq.json
+  cp /vagrant/sensu/client.json /etc/sensu/conf.d/client.json
+  sed -i "s/HOSTNAME_HERE/${HOSTNAME}/g" /etc/sensu/conf.d/client.json
+  sed -i "s/IPADDR_HERE/${IPADDR}/g" /etc/sensu/conf.d/client.json
+  chown -R sensu:sensu /etc/sensu
+
+  update-rc.d sensu-client defaults
+}
+
+start_sensu(){
+  service sensu-client restart
+}
+
+prereqs
+install_sensu
+configure_sensu
+start_sensu
+
