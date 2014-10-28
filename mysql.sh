f5adb7f5d72204d809fb17f5c2321f8c07362b48
W Chan <m4d.coder@gmail.com>
2014-10-28 16:19:08 -0700

    Move mistral setup to st2_deploy

--- mysql.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password StackStorm'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password StackStorm'
-apt-get update
-apt-get install -y mysql-server
-service mysql restart
